ec7772b62289fbb0ecce0766c6bbdb8bfea6ca6d
Josian Velez Ramos <josian.velez@upr.edu>
2018-07-01 16:43:54 -0400

    Add files via upload

---
+++ src/Main/Launch.java
@@ -0,0 +1,14 @@
+package Main;
+
+
+/**
+ * Created by AlexVR on 7/1/2018.
+ */
+
+public class Launch {
+
+    public static void main(String[] args) {
+        GameSetUp game = new GameSetUp("BaseGame", 600, 800);
+        game.start();
+    }
+}
